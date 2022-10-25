4db22c69d80aa94ce10b662c314dd7d496050f9a
Tsumioshino <marcus.oliveira@icen.ufpa.br>
2022-10-25 10:59:22 -0300

    update: some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     'grafo/templates/*.html',
     './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            stone: colors.warmGray,
+            sky: colors.lightBlue,
+            neutral: colors.trueGray,
+            gray: colors.coolGray,
+            slate: colors.blueGray,
+        }
+    }
   },
   plugins: [
     require('tw-elements/dist/plugin'),
